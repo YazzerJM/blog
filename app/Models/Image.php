50e25c11730de422fc304e6d9f02a0b4cbcec66b
YazzerJM <yasserjimenezmartinez@hotmail.com>
2021-07-21 18:01:30 -0500

    Preparando la BBDD

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Image extends Model
 {
     use HasFactory;
 
+    // Relacion Polimorfica
 
-    public function posts(){
-        return $this->hasMany(Post::class);
+    public function imageble(){
+        return $this->morphTo();
     }
 }
---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // 1 a muchos inversa
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relacion muchos a muchos
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // relacion 1 a 1 polimorfica
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+}
