e5c09071f5499586b31dc6b125affd7ac1cb65d8
YazzerJM <yasserjimenezmartinez@hotmail.com>
2021-07-22 03:34:35 -0500

    Factories agregados para informacion ficticia

--- database/migrations/2021_07_21_224152_create_images_table.php
+++ database/migrations/2021_07_21_224152_create_images_table.php
@@ -19,7 +19,7 @@ class CreateImagesTable extends Migration
             $table->string('url');
 
             $table->unsignedBigInteger('imageable_id');
-            $table->unsignedBigInteger('imageable_type');
+            $table->string('imageable_type');
 
             $table->timestamps();
         });
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Yasser Jimenez Martinez',
+            'email' => 'yazzer.jimenez@gmail.com',
+            'password' => bcrypt('123123')
+        ]);
+
+        User::factory(99)->create();
+    }
+}
