833564dc20aa7f0e2547272c0c7c54a89316da45
YazzerJM <yasserjimenezmartinez@hotmail.com>
2021-07-27 23:46:21 -0500

    Creado los posts creados por usuario

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\TagController;
 
 // Route::get('admin', [])->name('admin');
@@ -13,3 +14,5 @@ Route::get('', [HomeController::class, 'index'])->name('admin.home');
 Route::resource('categories', CategoryController::class)->names('admin.categories');
 
 Route::resource('tags', TagController::class)->names('admin.tags');
+
+Route::resource('posts', PostController::class)->names('admin.posts');
