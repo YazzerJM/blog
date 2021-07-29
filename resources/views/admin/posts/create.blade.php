a27391495def193c97c45561ccd6e27f06c2bd0d
YazzerJM <yasserjimenezmartinez@hotmail.com>
2021-07-28 21:22:05 -0500

    Formulario para crear un post y se agrego plugin CKeditor5 texto enriquecido

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,11 +3,70 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Crear categorias</h1>
+    <h1>Crear nuevo post</h1>
 @stop
 
 @section('content')
-    <p>Parte administrativa del blog</p>
+    <div class="card">
+        <div class="card-body">
+
+            {!! Form::open(['route' => 'admin.posts.store', 'autocomplete' => 'off']) !!}
+
+                <div class="form-group">
+                    {!! Form::label('name', 'Nombre:') !!}
+                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre del post']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('slug', 'Slug:') !!}
+                    {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre del slug', 'readonly']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('category_id', 'Categoria') !!}
+                    {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    <p class="font-weight-bold">Etiquetas</p>
+
+                    @foreach ($tags as $tag)
+                        <label class="mr-2">
+                            {!! Form::checkbox('tags[]', $tag->id, null) !!}
+                            {{$tag->name}}
+                        </label>
+                    @endforeach
+                </div>
+
+                <div class="form-group">
+                    <p class="font-weight-bold">Estado</p>
+
+                    <label class="mr-2">
+                        {!! Form::radio('status', 1, true) !!}
+                        Borrador
+                    </label>
+
+                    <label>
+                        {!! Form::radio('status', 2) !!}
+                        Publicado
+                    </label>
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('extract', 'Extracto:') !!}
+                    {!! Form::textarea('extract', null, ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('body', 'Cuerpo del post:') !!}
+                    {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('Crear post', ['class' => 'btn btn-primary']) !!}
+
+            {!! Form::close() !!}
+        </div>
+    </div>
 @stop
 
 @section('css')
@@ -15,5 +74,32 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+    <script src="{{asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js')}}"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
+
+    <script>
+        $(document).ready( function() {
+            $("#name").stringToSlug({
+                setEvents: 'keyup keydown blur',
+                getPut: '#slug',
+                space: '-'
+            });
+        });
+    </script>
+
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#extract' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#body' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+@endsection
